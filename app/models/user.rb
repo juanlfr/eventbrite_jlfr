62e71a3570471df3e3388ead1677273209fc96bd
juanlfr <fuentes0091@gmail.com>
2019-11-05 18:22:53 +0100

    erase the welcome mail function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :welcome_send
+  #after_create :welcome_send
 
 	#validates :first_name, presence: true
   #validates :last_name, presence: true
